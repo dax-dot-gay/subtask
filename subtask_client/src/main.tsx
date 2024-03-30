c883e06d24673e841843abac705951c26b6c533d
Dax Harris <matteovh@gmail.com>
2024-03-30 09:30:19 -0400

    setup https

--- subtask_client/src/main.tsx
+++ subtask_client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/code-highlight/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/tiptap/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
